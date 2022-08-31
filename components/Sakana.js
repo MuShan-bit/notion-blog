ce0485b7485faad6b1f8257dc2289c0a00d4e903
MuShan <mushanbit@163.com>
2022-08-31 16:45:11 +0800

    Update Sakana.js

--- components/Sakana.js
+++ components/Sakana.js
@@ -4,11 +4,33 @@ import { useGlobal } from '@/lib/global'
 import { loadExternalResource } from '@/lib/utils'
 import { useEffect } from 'react'
 
+export default function Sakana() {
+  if (!BLOG.SAKANA_PET) {
+    return <></>
+  }
+  const { switchTheme } = useGlobal()
+
+  function handleClick() {
+    if (BLOG.WIDGET_PET_SWITCH_THEME) {
+      switchTheme()
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', initLive2D)
+    return () => {
+      window.removeEventListener('scroll', initLive2D)
+    }
+  }, [])
+
+  return <canvas id="live2d" className='cursor-pointer' width="280" height="250" onClick={handleClick} alt='切换主题' title='切换主题' />
+}
+
 /**
  * 加载宠物
  */
-function initLive2D() {
-  window.removeEventListener('scroll', initLive2D)
+function initSakana() {
+  window.removeEventListener('scroll', initSakana)
   setTimeout(() => {
     // 加载 waifu.css live2d.min.js waifu-tips.js
     if (screen.width >= 768) {
@@ -17,7 +39,7 @@ function initLive2D() {
         loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js', 'js')
       ]).then((e) => {
         // https://github.com/xiazeyu/live2d-widget-models
-        loadlive2d('live2d', BLOG.WIDGET_PET_LINK)
+        loadlive2d('Sakana', BLOG.WIDGET_PET_LINK)
       })
     }
   }, 300)
