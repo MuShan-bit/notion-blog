7f0cccdffeca3f84d4e241a9f2d668597c5712f8
MuShan <mushanbit@163.com>
2022-08-31 12:40:37 +0800

    Update Sakana.js

--- components/Sakana.js
+++ components/Sakana.js
@@ -1,28 +1,12 @@
-/* eslint-disable no-undef */
 import BLOG from '@/blog.config'
-import { useGlobal } from '@/lib/global'
 import { loadExternalResource } from '@/lib/utils'
-import { useEffect } from 'react'
 
 export default function Sakana() {
   if (!BLOG.SAKANA_WIDGET) {
     return <></>
   }
-  const { switchTheme } = useGlobal()
 
-  function handleClick() {
-    if (BLOG.WIDGET_PET_SWITCH_THEME) {
-      switchTheme()
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', initSakana)
-    return () => {
-      window.removeEventListener('scroll', initSakana)
-    }
-  }, [])
-
+  new SakanaWidget().mount('#sakana-widget');
   return <div id="sakana-widget"></div>
 }
 
@@ -31,5 +15,4 @@ export default function Sakana() {
  */
 function initSakana() {
   loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js', 'js');
-  new SakanaWidget().mount('#sakana-widget');
 }
