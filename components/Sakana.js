b6fcd2adc836137847a57ed16caaa87403d0f7d0
MuShan <mushanbit@163.com>
2022-08-31 16:10:38 +0800

    Update Sakana.js

--- components/Sakana.js
+++ components/Sakana.js
@@ -1,17 +1,10 @@
 import BLOG from '@/blog.config'
 
-export default function Sakana() {
-  if (!BLOG.SAKANA_WIDGET) {
-    return <></>
-  }
-  return <div id="sakana-widget"></div>
-}
-
 /**
  * 加载部件
  */
 function initSakana() {
   let sakana = document.createElement('script')
-  sakana.src = 'https://cdn.jsdelivr.net/npm/sakana-widget@2.2.1/lib/sakana.min.js'
+  sakana.src = BLOG.SAKANAWIDGET_LINK
   document.head.appendChild(sakana)
 }
